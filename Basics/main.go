80b99f6d4a0f81194484006ae2551bf2c94f9473
Chirag Nebhani <chiragnebhani0@gmail.com>
2024-09-14 12:46:49 +0530

    variables

--- Basics/main.go
+++ Basics/main.go
@@ -8,4 +8,32 @@ import (
 func main() {
 	fmt.Println("Learn Go Language by Hello World")
 	myutil.PrintMessage("Hello World")
+
+	// variables
+	// var name string = "Chirag"
+	// var version = "1.0"
+	// fmt.Println(name)
+	// fmt.Println(version)
+
+	// var currency = 34567
+	// fmt.Println("Currency is", currency)
+
+	// var num float64 = 1.65
+	// fmt.Println(num)
+
+	// var istrue bool = true
+	// fmt.Println(istrue)
+
+	// var golangDeveloper string = "Chirag Nebhani"
+	// fmt.Println(golangDeveloper)
+
+	// const pi = 3.14
+	// fmt.Println(pi)
+
+	person := "Chirag"
+	println(person)
+
+	var Public = "exportable"
+	var private = "not exportable"
+	fmt.Println(Public, private)
 }
